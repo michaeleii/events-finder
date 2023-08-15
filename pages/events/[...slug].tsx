0b18ff5fb4be31ee3e277ab5b6e8cb4a3091de12
Michael Lei <michaelleie0ca@gmail.com>
2023-08-15 02:13:12 -0700

    Add no filter message

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,6 +1,7 @@
 import EventCard from "@/components/EventCard";
 import { events } from "@/data/events";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 function FilteredEventsPage() {
   const router = useRouter();
@@ -13,13 +14,33 @@ function FilteredEventsPage() {
 
   return (
     <main className="max-w-4xl mx-auto p-5">
-      <div className="mb-20 mt-5"></div>
+      <div className="mb-20 mt-5">
+        <h1 className="text-5xl font-bold text-center">
+          {month} {year}
+        </h1>
+      </div>
       <ul className="space-y-5 max-w-3xl mx-auto">
-        {filteredEvents.map((event) => (
-          <li key={event.id}>
-            <EventCard event={event} />
-          </li>
-        ))}
+        {filteredEvents.length > 0 ? (
+          filteredEvents.map((event) => (
+            <li key={event.id}>
+              <EventCard event={event} />
+            </li>
+          ))
+        ) : (
+          <>
+            <p className="text-center text-3xl">
+              No events found for the chosen filter!
+            </p>
+            <div className="flex justify-center">
+              <Link
+                href="/events"
+                className="text-accent text-2xl underline underline-offset-4"
+              >
+                Go back
+              </Link>
+            </div>
+          </>
+        )}
       </ul>
     </main>
   );
