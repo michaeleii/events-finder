4a9c11baa9d80e4898e5e0b23c9d220859e933b4
Michael Lei <michaelleie0ca@gmail.com>
2023-08-18 22:21:39 -0700

    Add head

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,17 @@ import type { InferGetStaticPropsType, GetStaticProps } from "next";
 import EventCard from "@/components/EventCard";
 import { API_URL } from "@/helpers/constants";
 import { EventItem } from "@/interfaces/Event";
+import Head from "next/head";
 
 function Homepage({
   featuredEvents,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <main className="max-w-4xl mx-auto p-5">
+      <Head>
+        <title>Events Finder</title>
+        <meta name="description" content="Find fun and interesting events." />
+      </Head>
       <ul className="space-y-5 max-w-3xl mx-auto mt-10">
         {featuredEvents.map((event) => (
           <li key={event.id}>
