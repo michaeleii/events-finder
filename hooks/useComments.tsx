b5866e026257df89e74d6866d1c9172dbb1dddc7
Michael Lei <michaelleie0ca@gmail.com>
2023-08-23 16:51:54 -0700

    Finish adding api routes

--- hooks/useComments.tsx
+++ hooks/useComments.tsx
@@ -1,13 +1,14 @@
-import { API_URL } from "@/helpers/constants";
 import { useQuery } from "@tanstack/react-query";
 import { Comment } from "@/interfaces/Comment";
+import { useRouter } from "next/router";
 
 export default function useComments() {
+  const router = useRouter();
   const { isLoading, data: comments } = useQuery({
     queryKey: ["comments"],
     queryFn: async () => {
-      const res = await fetch(`/api/comments`);
-      const comments: Comment[] = await res.json();
+      const res = await fetch(`/api/comments/${router.query.id}`);
+      const { comments }: { comments: Comment[] } = await res.json();
       return comments;
     },
   });
--- hooks/useCreateComment.tsx
+++ hooks/useCreateComment.tsx
@@ -1,12 +1,13 @@
-import { API_URL } from "@/helpers/constants";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Comment } from "@/interfaces/Comment";
+import { useRouter } from "next/router";
 
 export default function useCreateComments() {
   const qc = useQueryClient();
+  const router = useRouter();
   const { mutate: createComment, isLoading: isCreatingComment } = useMutation({
     mutationFn: async (comment: Omit<Comment, "id">) => {
-      const res = await fetch(`api/comments`, {
+      const res = await fetch(`/api/comments/${router.query.id}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
